feee1e452f1d756470d142d3a04daf10b4c047f3
João <joaovitorsenac7817@gmail.com>
2024-08-10 12:27:44 -0300

    feat: edit RequestMapping and add CORS configuration

--- API/src/main/java/br/edu/senaisp/API/controller/UsuarioController.java
+++ API/src/main/java/br/edu/senaisp/API/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,8 @@ import br.edu.senaisp.API.repository.UsuarioRepository;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
 
 	@Autowired
